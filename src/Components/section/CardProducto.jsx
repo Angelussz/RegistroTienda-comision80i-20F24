2a45586cae5cccf3656d1ae6fcb2cfbf6fffd047
angelo <aperezr@unsa.edu.pe>
2024-02-28 11:53:46 -0500

    se corrigio el estilo de las cards de producto

--- src/Components/section/CardProducto.jsx
+++ src/Components/section/CardProducto.jsx
@@ -8,11 +8,15 @@ export const CardProducto = ({ producto }) => {
         <Card.Img variant="top" src="holder.js/100px180" />
         <Card.Body>
           <Card.Title>{producto.title}</Card.Title>
-          <Card.Text className="my-2">
+          {/* <Card.Text className="my-2">
             {producto.description}
           </Card.Text>
           <Card.Text className="fs-4">
             {producto.category}
+          </Card.Text> */}
+          <Card.Text>
+            <span className="mb-2 d-block">{producto.description}</span>
+            <span className="fs-4 d-block">{producto.category}</span>
           </Card.Text>
           <Button variant="primary">Ver mas</Button>
         </Card.Body>
